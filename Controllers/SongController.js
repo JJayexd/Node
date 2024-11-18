8e7d4474d60d4bfa6ddb9e70b37108ef5c189c8f
JJayexd <kontakt.emilbj@gmail.com>
2024-11-18 11:00:14 +0100

    ID

--- Controllers/SongController.js
+++ Controllers/SongController.js
@@ -3,8 +3,14 @@ import { SongModel } from "../Models/SongModel.js"
 
 export const SongController = express.Router();
 
-SongController.get('/songs', async (req, res) => {  
-    const songs = await SongModel.getSongs();  
-    res.send(songs);
-    console.log(songs);
+// SongController.get('/songs', async (req, res) => {  
+//     const songs = await SongModel.getSongs();  
+//     res.send(songs);
+//     console.log(songs);
+// })
+
+SongController.get('/songs/:id([0-9A-Za-z]*)', async (req, res) => {  
+    const single = await SongModel.getSongsById(req.params.id);  
+    res.send(single);
+    console.log(single);
 })  
--- Models/SongModel.js
+++ Models/SongModel.js
@@ -1,12 +1,39 @@
 import { supabase } from "../Config/cfgSupabase.js";
 import { ArtistModel } from "./ArtistModel.js";
 
+// export class SongModel {
+//     static getSongs = async() => {
+//         try {
+//             const { data, error } = await supabase
+//             .from('songs')
+//             .select('id, title, content, created_at, artist_id, artists ( name )');
+//         if (error) {
+//             throw new Error(error.message);
+//         }
+//         else {
+//             return data;
+//         }
+//     }
+//         catch(error) {
+//             console.log(error);
+//         }
+//     }
+// }
+
+
+/**
+ * 
+ * @param {string} id 
+ * @returns {Promise}
+ */
 export class SongModel {
-    static getSongs = async() => {
+    static getSongsById = async id => {
         try {
             const { data, error } = await supabase
             .from('songs')
-            .select('id, title, content, created_at, artist_id, artists ( name )');
+            .select('title')
+            .eq('id', id)
+            .single()
         if (error) {
             throw new Error(error.message);
         }
